7de2656e4a37ac922fe91e94a0351d402ae1840e
unknown <mtevfik41@gmail.com>
2021-08-09 17:36:21 +0300

    added expense list

--- ExpenseTrackerWithReact/src/components/Expenses/Expenses.js
+++ ExpenseTrackerWithReact/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2021');
@@ -17,18 +17,12 @@ const Expenses = (props) => {
     })
     filteredExpenses.sort((a,b) => b.date - a.date)
 
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {filteredExpenses.length === 0 ? <p className="warn">No Expenses Found</p> :
-                (filteredExpenses.map(expense => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />)))}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
