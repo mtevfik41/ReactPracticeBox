64e2ba14e91fe4e55584ee45ad7f7a3bf611470c
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-20 23:54:12 +0300

    error handling

--- ExampleProject/seasons/src/App.js
+++ ExampleProject/seasons/src/App.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
-  const [lat, setLat] = useState('');
-  const [lot, setLot] = useState('');
+  const [lat, setLat] = useState(null);
+  const [lot, setLot] = useState(null);
+  const [error,setError] = useState('');
 
   useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
@@ -13,15 +14,21 @@ const App = () => {
           setLot(position.coords.longitude);
         }
         ,
-        err => console.error(err),
+        err => setError(err.message),
         {timeout: 10000},
     );
   }, []);
 
   return (
       <div>
-        <h1>Latitude: {lat}</h1>
-        <h1>Longtitude: {lot}</h1>
+        {(!lat && !lot && !error) && <h1>Loading</h1>}
+        {error && <h1>User denied GeoLocation</h1>}
+        {(lat && lot) && (
+            <Fragment>
+              <h1>Latitude: {lat}</h1>
+              <h1>Longtitude: {lot}</h1>
+            </Fragment>
+          )}
       </div>
   );
 };
