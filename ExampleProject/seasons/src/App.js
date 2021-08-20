ad5f23f619ceabdd0b6199d4a5781d90c2f95be7
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-20 23:28:34 +0300

    using geolocation

--- ExampleProject/seasons/src/App.js
+++ ExampleProject/seasons/src/App.js
@@ -1,26 +1,29 @@
-import React, {useState} from 'react';
-import SeasonDisplay from './components/SeasonDisplay';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
   const [lat, setLat] = useState('');
   const [lot, setLot] = useState('');
-  window.navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(position)
-        setLat(position.coords.latitude);
-        setLot(position.coords.longitude)}
-      ,
-      err => console.error(err),
-      {timeout:10000}
-  )
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+          setLat(position.coords.latitude);
+          setLot(position.coords.longitude);
+        }
+        ,
+        err => console.error(err),
+        {timeout: 10000},
+    );
+  }, []);
+
   return (
-    <div>
-      <h1>Latitude: {lat}</h1>
-      <h1>Longtitude: {lot}</h1>
-      <SeasonDisplay/>
-    </div>
+      <div>
+        <h1>Latitude: {lat}</h1>
+        <h1>Longtitude: {lot}</h1>
+      </div>
   );
-}
+};
 
 export default App;
