d428440a4c7d8061dee7d664e2cc0e4c0cdad240
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-22 20:59:35 +0300

    styled components

---
+++ ExampleProject/react-styled-components/src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React, {Fragment} from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav,NavLogo,NavbarContainer,MobileIcon,NavMenu,NavLinks,NavItem,NavBtnLink,NavBtn} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+      <Fragment>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <span className="fromLeft">dolla</span>
+            </NavLogo>
+            <MobileIcon>
+              <FaBars/>
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to="about">
+                  <span className="fromLeft">About</span>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="discover">
+                  <span className="fromLeft">Discover</span>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">
+                  <span className="fromLeft">Services</span>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="signup">
+                  <span className="fromLeft">Sign Up</span>
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/signin">
+                Sign In
+              </NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </Fragment>
+  )
+}
+
+export default Navbar;
